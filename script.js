f02937806e45f5804363d4160decd1c854c3928d
KorYme <maxime.corby.pro@gmail.com>
2026-04-18 22:40:30 +0200

    First iteration

--- script.js
+++ script.js
@@ -1,47 +1,28 @@
-const path = window.location.pathname;
-
 fetch('projects.json')
-  .then(res => res.json())
-  .then(data => {
-
-    // HOME PAGE
-    if (document.getElementById('gallery')) {
+    .then(res => res.json())
+    .then(data => {
       const gallery = document.getElementById('gallery');
 
-      data.forEach(project => {
-        const card = document.createElement('a');
-        card.href = `project.html?id=${project.id}`;
-        card.className = 'project-card';
+      data.forEach((project, index) => {
 
-        card.innerHTML = `
+        const row = document.createElement('div');
+        row.className = 'project-row';
+
+        if (index % 2 !== 0) row.classList.add('reverse');
+
+        row.innerHTML = `
+        <a href="project.html?id=${project.id}" class="project-img">
           <img src="${project.cover}" loading="lazy">
-          <div class="overlay">${project.title}</div>
-        `;
+        </a>
 
-        gallery.appendChild(card);
+        <div class="project-info">
+          <div>
+            <h3>${project.title}</h3>
+            <p>${project.description}</p>
+          </div>
+        </div>
+      `;
+
+        gallery.appendChild(row);
       });
-    }
-
-    // PROJECT PAGE
-    if (path.includes('project.html')) {
-      const params = new URLSearchParams(window.location.search);
-      const id = params.get('id');
-
-      const project = data.find(p => p.id === id);
-
-      if (!project) return;
-
-      document.getElementById('title').textContent = project.title;
-      document.getElementById('description').textContent = project.description;
-      document.getElementById('hero').src = project.hero;
-
-      const gallery = document.getElementById('projectGallery');
-
-      project.images.forEach(src => {
-        const img = document.createElement('img');
-        img.src = src;
-        img.loading = 'lazy';
-        gallery.appendChild(img);
-      });
-    }
-  });
+    });
